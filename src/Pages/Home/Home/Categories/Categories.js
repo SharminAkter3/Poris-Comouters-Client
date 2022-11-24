f4afa201ce8bad6be4360c07ea62a17c47fa98fa
SharminAkter3 <mssharmin4145@gmail.com>
2022-11-24 20:01:39 +0600

    added-categories-section

--- src/Pages/Home/Home/Categories/Categories.js
+++ src/Pages/Home/Home/Categories/Categories.js
@@ -1,33 +1,33 @@
-// import React, { useEffect, useState } from 'react';
-// import CategoriesCard from './CategoriesCard';
+import React, { useEffect, useState } from 'react';
+import CategoriesCard from './CategoriesCard';
 
-// const Categories = () => {
-//     const [categories, setCategories] = useState([]);
-//     useEffect(() => {
-//         fetch('categories.json')
-//             .then(res => res.json())
-//             .then(data => {
-//                 console.log(data)
-//                 setCategories(data)
-//             })
-//     }, [])
+const Categories = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setCategories(data)
+            })
+    }, [])
 
-//     return (
-//         <div>
-//             <div>
-//                 <h2 className='text-3xl text-blue-700 font-semibold text-center m-5'>Product Categories</h2>
-//             </div>
-//             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 m-10' >
-//                 {
-//                     categories.map(category => <CategoriesCard
-//                         key={category._id}
-//                         category={category}
-//                     ></CategoriesCard>)
-//                 }
-//             </div>
-//         </div>
+    return (
+        <div>
+            <div>
+                <h2 className='text-3xl text-blue-700 font-semibold text-center m-5'>Product Categories</h2>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 m-10' >
+                {
+                    categories.map(category => <CategoriesCard
+                        key={category._id}
+                        category={category}
+                    ></CategoriesCard>)
+                }
+            </div>
+        </div>
 
-//     );
-// };
+    );
+};
 
-// export default Categories;
+export default Categories;
