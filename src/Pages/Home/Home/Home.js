3ef856287366b4eaaef451ba6eea4835c1a4cc65
SharminAkter3 <mssharmin4145@gmail.com>
2022-11-24 19:35:55 +0600

    add-home-page

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,18 +1,18 @@
-// import React from 'react';
-// import About from './About/About';
-// import AdvertiesProducts from './AdvertisesProducts/AdvertiesProducts';
-// import Banner from './Banner/Banner';
-// import Categories from './Categories/Categories';
+import React from 'react';
+import About from './About/About';
+import AdvertiesProducts from './AdvertisesProducts/AdvertiesProducts';
+import Banner from './Banner/Banner';
+import Categories from './Categories/Categories';
 
-// const Home = () => {
-//     return (
-//         <div>
-//             <Banner></Banner>
-//             <About></About>
-//             <Categories></Categories>
-//             <AdvertiesProducts></AdvertiesProducts>
-//         </div>
-//     );
-// };
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <About></About>
+            <Categories></Categories>
+            <AdvertiesProducts></AdvertiesProducts>
+        </div>
+    );
+};
 
-// export default Home;
+export default Home;
